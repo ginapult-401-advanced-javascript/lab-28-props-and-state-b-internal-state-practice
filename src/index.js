555958383895127bce4e229e6c41b3e1ada1bb12
Gina Pultorak Scarpello <50841963+ginapult@users.noreply.github.com>
2019-09-18 16:47:40 -0700

    internal state practice part 1

--- src/index.js
+++ src/index.js
@@ -4,15 +4,62 @@ import ReactDOM from 'react-dom';
 import './styles.css';
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Gina',
+      count: 0,
+    };
+  }
+
+  handleNameChange = (input) => {
+    this.setState(previousState => {
+      return {
+        
+      }
+    })
+  }
+
+   render() {
     return (
       <React.Fragment>
         <div>
-          <div>Name: </div>
-          <div># of Updates: </div>
+          <div>Name: {this.state.name}</div>
+          <div># of Updates: {this.state.count}</div>
+          <Form />
         </div>
       </React.Fragment>
     );
+  }
+}
+
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Gina',
+    };
+  }
+
+  handleChange = event => {
+    this.state.setState({
+      name:event.target.value,
+    })
+  }
+
+  render() {
+    return(
+      <React.Fragment>
+        <form>
+          <input
+            type="text"
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </React.Fragment>
+    )
   }
 }
 
